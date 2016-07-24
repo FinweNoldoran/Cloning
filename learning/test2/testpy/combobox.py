c96a3f5d3003aa7479eae74c872db03eb5e5da1f
FinweNoldoran <FinweNoldoran@users.noreply.github.com>
2016-07-23 23:34:22 -0700

    removed learning

--- learning/test2/testpy/combobox.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets, QtCore
-
-
-class ComboBox(QtWidgets.QComboBox):
-    popupAboutToBeShown = QtCore.pyqtSignal()
-
-    def showPopup(self):
-        self.popupAboutToBeShown.emit()
-        super(ComboBox, self).showPopup()
