b067d06266008c64a77146ce789900d053085cf2
FinweNoldoran <FinweNoldoran@users.noreply.github.com>
2016-08-04 10:18:22 +0100

    version 0.0.03

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ matplotlib.use('Qt5Agg')
 import pydna
 from gelwindow import myplots
 
-CURRENT_VERSION = '0.0.02'
+CURRENT_VERSION = '0.0.03'
 
 '''
 CloneApp main python file. Main window is defined here.
